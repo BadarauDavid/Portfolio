8572577c315760e532d97e80228cd05448ffc8df
BadarauDavid <badaraudavidiulian@gmail.com>
2024-03-20 13:01:03 +0200

    eliminate duplicate code

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,44 +1,45 @@
 export default function Education() {
+  const allEducation = [
+    {
+      name: "CodeCool(FullStack)",
+      year: "(2022-2023)",
+      skills:
+        " Java, SpringBoot, JavaScript, React, Postgresql, MongoDb, CSS, HTML, Node, Git, Tailwind, Effective Communication, Collaboration, Curiosity and Continuous Learning, Attention to Detail, Presentation Skills",
+    },
+    {
+      name: "University „Alexandru Ioan Cuza”(Management)",
+      year: "(2019-Present)",
+      skills:
+        "   Communication, Time Management, Teamwork, Problem-Solving, Adaptability, Empathy, Creativity, Stress Management, Continuous Learning",
+    },
+    {
+      name: "College „Anghel Rugină”(Mathematics and Computer Science)",
+      year: "(2010-2014)",
+      skills:
+        " C++, Analytical Thinking, Problem-Solving, Logic, Patience andPerseverance, Mathematical Skills, Collaboration, Self-Learning, TechnicalCommunication, Curiosity",
+    },
+  ];
+
   return (
     <div className="py-10  dark:text-white dark:bg-[#232325] bg-gray-100">
       <h2 className="mb-8 text-3xl text-white text-center">
         My <span>Education</span>
       </h2>
-
-      <div className="mb-[20px] text-black dark:text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
-        <p>CodeCool(FullStack)</p>
-        <p className="dark:text-gray-400 text-gray-800">(2022-2023) </p>
-        <p className="dark:text-gray-500 text-gray-700">
-          Java, SpringBoot, JavaScript, React, Postgresql, MongoDb, CSS, HTML,
-          Node, Git, Tailwind,Effective Communication,Collaboration,Curiosity
-          and Continuous Learning, Attention to Detail, Presentation Skills
-        </p>
-      </div>
-
-      <div className="h-[50px] w-[2px] bg-gray-400 relative my-1 mx-auto"></div>
-
-      <div className="mb-[20px] text-black dark:text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
-        <p>University „Alexandru Ioan Cuza”(Management)</p>
-        <p className="dark:text-gray-400 text-gray-800 ">(2019-Present) </p>
-        <p className="dark:text-gray-500 text-gray-700 ">
-          {" "}
-          Communication, Time Management,
-          Teamwork,Problem-Solving,Adaptability,Empathy,Creativity,Stress
-          Management,Continuous Learning
-        </p>
-      </div>
-
-      <div className="h-[50px] w-[2px] bg-gray-400 relative my-1 mx-auto"></div>
-
-      <div className="mb-[20px] text-black dark:text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
-        <p>College "Anghel Rugină"(Mathematics and Computer Science)</p>
-        <p className="dark:text-gray-400 text-gray-800 ">(2010-2014)</p>
-        <p className="dark:text-gray-500 text-gray-700 ">
-          C++, Analytical Thinking, Problem-Solving, Logic,Patience and
-          Perseverance,Mathematical Skills,Collaboration,Self-Learning,Technical
-          Communication,Curiosity
-        </p>
-      </div>
+      {allEducation &&
+        allEducation.map((e,index) => (
+          <>
+            <div className="mb-[20px] text-black dark:text-white relative bg-gray-800/20 p-4 rounded-3xl max-w-[300px] sm:max-w-[600px] mx-auto">
+              <p>{e.name}</p>
+              <p className="dark:text-gray-400 text-gray-800">{e.year}</p>
+              <p className="dark:text-gray-500 text-gray-700">{e.skills}</p>
+            </div>
+          
+          
+            {index !== allEducation.length - 1 && (
+        <div className="h-[50px] w-[2px] bg-gray-400 relative my-1 mx-auto"></div>
+      )}
+          </>
+        ))}
     </div>
   );
 }
